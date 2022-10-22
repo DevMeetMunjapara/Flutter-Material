9aedc9356662b2806abfc1b369a814e9497a557b
Meet <meetmunjapara212@gmail.com>
2022-10-22 16:24:51 +0530

    drawer

--- lib/main.dart
+++ lib/main.dart
@@ -9,19 +9,23 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  myfun() {
+    print('Hello');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          iconTheme: IconThemeData(color: Colors.black),
+          iconTheme: IconThemeData(color: Colors.white),
           title: Text(
             'AppBar',
-            style: TextStyle(color: Colors.black),
+            style: TextStyle(color: Colors.white),
           ),
           centerTitle: true,
-          backgroundColor: Colors.yellow,
+          backgroundColor: Colors.deepPurple[400],
           toolbarHeight: 100,
           shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.only(
@@ -43,7 +47,61 @@ class MyApp extends StatelessWidget {
             ),
           ],
         ),
-        drawer: Drawer(),
+        drawer: Drawer(
+          child: Container(
+            color: Colors.deepPurple[100],
+            child: ListView(
+              children: <Widget>[
+                DrawerHeader(
+                    child: Row(
+                  children: const [
+                    CircleAvatar(
+                      backgroundImage: AssetImage('img/meet.jpg'),
+                      radius: 40,
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      'Meet Munjapara\n@meet_munjapara',
+                      style: TextStyle(fontSize: 20, color: Colors.black54),
+                    ),
+                  ],
+                )),
+                ListTile(
+                  onTap: () {
+                    print('Profile');
+                  },
+                  contentPadding: EdgeInsets.fromLTRB(25, 0, 0, 0),
+                  leading: Icon(
+                    Icons.account_circle,
+                    size: 40,
+                    color: Colors.black,
+                  ),
+                  title: Text(
+                    'Profile',
+                    style: TextStyle(fontSize: 18, color: Colors.black),
+                  ),
+                ),
+                ListTile(
+                  onTap: () {
+                    print('Setting');
+                  },
+                  contentPadding: EdgeInsets.fromLTRB(25, 0, 0, 0),
+                  leading: Icon(
+                    Icons.settings,
+                    size: 40,
+                    color: Colors.black,
+                  ),
+                  title: Text(
+                    'Setting',
+                    style: TextStyle(fontSize: 18, color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
